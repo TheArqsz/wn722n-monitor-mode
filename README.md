ce07fe60af7e70d4dc0ab389bb7fc2da28cba5a0
TheArqsz <arek.maruszczak@gmail.com>
2020-06-13 14:45:15 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# wn722n-monitor-mode
-Script that sets up monitor mode on TPLink wn722n V2/V3
+# Script for monitor mode in TPLink wn722n v2/v3 network adapter
+
+## Prerequisites
+
+All you need is:
+
+- git
+- iwconfig
+- ifconfig (`net-tools`)
+- access to `root`/`sudo`
+
+## Install
+
+All you have to do is:
+
+- connect your adapter to USB (or make sure it is connected to virtual machine)
+- execute commands:
+```console
+git clone https://github.com/TheArqsz/wn722n-monitor-mode
+cd wn722n-monitor-mode
+chmod +x run.sh
+./run.sh <network interface>
+```
---
+++ run.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+	echo
+	echo "Usage: sudo ./run.sh <wifi_interface>"
+	echo
+	echo "e.g. sudo ./run.sh wlan0"
+	echo
+	exit 
+fi
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Run as root/sudo"
+	exit
+fi
+
+if [ ! -d "/sys/class/net/$1" ]; then
+	echo "Error: interface '$1' does not exist"
+	exit
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+	echo "Error: git not installed!"
+	echo "Install it with your package manager"
+	exit
+fi
+
+tmp_dir=`mktemp -d`
+echo -e "Creating tmp directory: $tmp_dir"
+
+if [ ! -d $tmp_dir ]; then
+	echo "Error: something went wrong while creating tmp directory"
+	exit 
+fi
+
+user="aircrack-ng"
+repo="rtl8188eus"
+user="TheArqsz"
+repo="test_repository_32187"
+echo "Collecting important files from: $user/$repo"
+git clone https://github.com/$user/$repo $tmp_dir
+
+echo "Cleaning system"
+rmmod r8188eu.ko 2>/dev/null
+cd $tmp_dir
+make uninstall 2>/dev/null
+make clean 2>/dev/null
+
+echo "Installing driver"
+echo "blacklist r8188eu.ko" > /etc/modprobe.d/realtek.conf
+make && make install
+modprobe 8188eu
+
+echo "Setting up monitor mode"
+ifconfig $1 down
+status=$(iwconfig $1 mode monitor)
+if [[ ! "$status" == "" ]]; then
+	echo "Error: cannot set up monitor mode for $1"
+fi
+ifconfig $1 up
+
+echo "Removing tmp directory"
+rm -r $tmp_dir
+
+echo "If device disconnected from system:"
+echo "reconnect it to usb and:"
+echo "	ifconfig $1 down"
+echo "	iwconfig $1 mode monitor"
+echo "	ifconfig $1 up"
